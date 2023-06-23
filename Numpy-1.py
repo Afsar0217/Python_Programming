4401cbf2a90d95395e2eb112a554be3c8158d7c8
Afsar0217 <137322540+Afsar0217@users.noreply.github.com>
2023-06-23 15:21:51 +0530

    Create Numpy-1.py

---
+++ Numpy-1.py
@@ -0,0 +1,10 @@
+import numpy as np
+#Accessing array elements
+a=np.array([1,2,3,4,5,6])
+print('Element at index 1 is',a[1])
+#Accessing ndarray elements
+b=np.array([[1,2,3],
+            [4,5,6],
+            [7,8,9]])
+print('Access a row ',b[0])
+print('Access an element',b[1,2])
