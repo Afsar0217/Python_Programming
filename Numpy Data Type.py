13abffcb2af3e02ff54f17a7bad87137df24f779
Afsar0217 <137322540+Afsar0217@users.noreply.github.com>
2023-06-23 15:37:23 +0530

    Create Numpy Data Type.py

---
+++ Numpy Data Type.py
@@ -0,0 +1,17 @@
+#Numpy Data Types
+import numpy as np
+#Integer Data Types
+a=np.array([1,2,3,4,5])
+print(a.dtype)
+#Floating Point Data Types
+b=np.array([1.4,2.7,3.44,4.0,5.1])
+print(b.dtype)
+#Boolean Data Types
+c=np.array([(10<20)])
+print(c.dtype)
+#Complex Data Types
+d=np.array([1+6j,2+3j,3+7j])
+print(d.dtype)
+#String Data Types
+e=np.array(['Apple','Ball','Cat'])
+print(e.dtype)
