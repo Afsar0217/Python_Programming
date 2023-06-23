eaf1aea786b09326d73204495cb9f65a7ba4d7bd
Afsar0217 <137322540+Afsar0217@users.noreply.github.com>
2023-06-23 15:12:59 +0530

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -1,3 +1,9 @@
 import numpy as np
-a=np.array([1,2,3])
+#1-D numpy array
+a=np.array([1,2,3,4,5])
 print(a)
+print(type(a))
+#2-D numpy array
+b=np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(b)
+print(type(b))
